9f4ee13e2ece239a27bc162a2a8723a3026a223e
technodot <techdudie@mail.com>
2026-02-22 20:17:20 -0600

    feat: add capacitor build targets

---
+++ android/app/src/main/java/org/ftcvanguard/vanguard/MainActivity.java
@@ -0,0 +1,5 @@
+package org.ftcvanguard.vanguard;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ electron/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.ftcvanguard.vanguard',
+  appName: 'Vanguard',
+  webDir: 'build'
+};
+
+export default config;
