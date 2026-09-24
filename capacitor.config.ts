773374b0615c99f403607bbd14c0d5309b6195a9
TechnoDot <73961295+TechDudie@users.noreply.github.com>
2026-09-24 10:02:49 -0500

    revert: i'm not doing sveltekit+capacitor

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-	appId: 'org.ftcvanguard.vanguard',
-	appName: 'Vanguard',
-	webDir: 'build'
-};
-
-export default config;
