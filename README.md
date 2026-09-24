c348f09de76169eb8bc62db7b96dbf3894127903
TechnoDot <73961295+TechDudie@users.noreply.github.com>
2026-09-24 11:09:57 -0500

    feat(ios): react native+expo

--- README.md
+++
@@ -1,3 +0,0 @@
-# ftcvanguard
-
-Palantir for tech challenges
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ backgroundColor: '#000000' }}>
+    </View>
+  );
+}
